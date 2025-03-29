6bdf6da76be4d46f4590483d0c2b6a575d6edc42
tony1224 <starmine2013@gmail.com>
2025-03-30 01:18:35 +0900

    create SwiftWorkshopDemo

---
+++ SwiftWorkshopDemo/SwiftWorkshopDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWorkshopDemo
+//
+//  Created by Jun Morita on 2025/03/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftWorkshopDemo/SwiftWorkshopDemo/SwiftWorkshopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWorkshopDemoApp.swift
+//  SwiftWorkshopDemo
+//
+//  Created by Jun Morita on 2025/03/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWorkshopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
